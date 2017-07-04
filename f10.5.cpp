e41221842b9bd9f80dcca0946e1d477ce1850e21
liuchenjane <2386418764@qq.com>
2017-07-04 22:01:42 +0800

    一个僵尸进程

--- f10.5.cpp
+++ f10.5.cpp
@@ -3,11 +3,12 @@
 #include <stdlib.h>
 int main(){
     pid_t pid = fork();
-    if (pid > 0){
+    if (pid > 0){//父进程
         printf("in parent process, sleep for one miniute .. \n" );
         sleep(3);
         printf("after sleeping, and exit!\n" );
     }else if(pid == 0){
+        //子进程退出，成为一个僵尸进程
         printf("in child process, and exit\n" );
         exit(0);
     }

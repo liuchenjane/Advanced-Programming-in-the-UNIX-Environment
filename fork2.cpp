97ac88f466d73291e68a70abe5877ba82c53b82d
liuchenjane <2386418764@qq.com>
2017-07-04 20:40:40 +0800

    Update fork2.cpp

--- fork2.cpp
+++ fork2.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-int global = 1;
+int global = 1;//全局变量，存储在data段
 int main(){
     pid_t pid;
-    int stack = 1;
+    int stack = 1;//局部变量，存储在栈区
     int *heap;
-    heap = (int *)malloc(sizeof(int));
+    heap = (int *)malloc(sizeof(int));//heap指向动态分配的内存区域，在堆区
     *heap=3;
     pid = fork();
     if(pid<0){
@@ -20,7 +20,7 @@ int main(){
         printf("In sub-process, global: %d, stack: %d, heap: %d\n",global,stack,*heap );
         exit(0);
     }else{
-        sleep(2);
+        sleep(2);//休眠2s，确保子进程执行完毕，再执行父进程
         printf("In parent-process, global: %d, stack: %d, heap: %d\n",global,stack,*heap );
     }
     return 0;

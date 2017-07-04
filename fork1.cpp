059a8548382155c2683a2f834c2deca6a1a5a7ab
liuchenjane <2386418764@qq.com>
2017-07-04 20:15:18 +0800

    first fork

---
+++ fork1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(){
+    pid_t pid;
+    pid = fork();
+    if (pid < 0){
+        perror("fail to fork");
+        exit(-1);
+    }
+    else if(pid == 0){
+        printf("sub-process, PID: %u, PPID: %u\n",getpid(),getppid());
+    }
+    else{
+        printf("Parent, PID: %u, Sub-process PID:%u\n",getpid(),pid );
+            sleep(2);
+    }
+    return 0;
+}

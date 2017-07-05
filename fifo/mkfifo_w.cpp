5c6ccce9525b4e3e695418d6bed3970fe9f2fe58
liuchenjane <2386418764@qq.com>
2017-07-05 17:20:15 +0800

    Update mkfifo_w.cpp

--- fifo/mkfifo_w.cpp
+++ fifo/mkfifo_w.cpp
@@ -7,8 +7,8 @@ int main(int argc, char ** argv){
     if(argc<2){
         printf("please input the write data.\n" );
     }
-    fd= open(P_FIFO,O_WRONLY|O_NONBLOCK);
-    write(fd,argv[1],100);
+    fd= open(P_FIFO,O_WRONLY|O_NONBLOCK);//以可写，非阻塞方式打开FIFO
+    write(fd,argv[1],100);//将argv[1]写入fd
     close(fd);
     return 0;
 }

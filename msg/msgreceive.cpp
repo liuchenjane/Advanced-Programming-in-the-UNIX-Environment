aa4a6171b08c17fed1cc8981a86f530b596e978e
liuchenjane <2386418764@qq.com>
2017-07-05 20:23:47 +0800

    Update msgreceive.cpp

--- msg/msgreceive.cpp
+++ msg/msgreceive.cpp
@@ -13,12 +13,13 @@ int main(){
     int msgid=-1;
     struct msg_st data;
     long int msgtype=0;
-
+    //建立消息队列
     msgid = msgget((key_t)1234, 0666|IPC_CREAT);
     if (msgid == -1){
         fprintf(stderr,"msgget failed with error: %d\n", errno);
         exit(EXIT_SUCCESS);
     }
+    //从队列中获取消息
     while(running){
         if(msgrcv(msgid,(void*)&data, BUFSIZ, msgtype,0)==-1){
             fprintf(stderr, "msgrcv failed with errno: %d\n",errno);
@@ -29,7 +30,7 @@ int main(){
             running=0;
         }
     }
-
+        //删除消息队列
     if(msgctl(msgid, IPC_RMID,0)==-1){
         fprintf(stderr, "msgctl(IPC_RMID) failed\n");
         exit(EXIT_FAILURE);

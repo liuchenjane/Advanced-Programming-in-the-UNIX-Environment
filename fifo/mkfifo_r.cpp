9a1388c10c018cdb495a74556502f1463b4507df
liuchenjane <2386418764@qq.com>
2017-07-05 17:18:35 +0800

    Update mkfifo_r.cpp

--- fifo/mkfifo_r.cpp
+++ fifo/mkfifo_r.cpp
@@ -10,16 +10,17 @@ int main(){
     char cache[1000];
     int fd;
     memset(cache,0,sizeof(cache));
-    if(access(P_FIFO,F_OK)==0){
+    if(access(P_FIFO,F_OK)==0){//若管道文件存在，则删掉
         execlp("rm","-f",P_FIFO,NULL);
         printf("access.\n" );
     }
-    if(mkfifo(P_FIFO,0777)<0){
+    if(mkfifo(P_FIFO,0777)<0){//创建一个有名管道
         printf("ceatenamed pipe failed\n" );
     }
-    fd=open(P_FIFO, O_RDONLY | O_NONBLOCK);
+    fd=open(P_FIFO, O_RDONLY | O_NONBLOCK);//以非阻塞只读方式打开管道
     while(1){
         memset(cache,0,sizeof(cache));
+        //读数据
         if((read(fd,cache,100))==0){
             printf("nodata:\n" );
         }else{

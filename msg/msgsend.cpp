3dbd8f1b9d07ad7af01de82f612420b1e80370fb
liuchenjane <2386418764@qq.com>
2017-07-05 20:19:13 +0800

    Add files via upload

---
+++ msg/msgsend.cpp
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/msg.h>
+#define MAX_TEXT 512
+
+struct msg_st {
+    long int msg_type;
+    char text[MAX_TEXT];
+};
+int main(){
+    int running =1;
+    int msgid=-1;
+    struct msg_st data;
+    char buffer[BUFSIZ];
+    msgid = msgget((key_t)1234, 0666|IPC_CREAT);
+    if (msgid == -1){
+        fprintf(stderr,"msgget failed with error: %d\n", errno);
+        exit(EXIT_SUCCESS);
+    }
+    while(running){
+        printf("Enter some text:" );
+        fgets(buffer, BUFSIZ, stdin);
+        data.msg_type=1;
+        strcpy(data.text, buffer);
+        if(msgsnd(msgid,(void *)&data, MAX_TEXT,0)==-1){
+            fprintf(stderr, "msgsnd failed\n");
+                exit(EXIT_FAILURE);
+        }
+
+        if(strncmp(data.text, "end",3)==0){
+            running=0;
+        }
+    }
+
+    if(msgctl(msgid, IPC_RMID,0)==-1){
+        fprintf(stderr, "msgctl(IPC_RMID) failed\n");
+        exit(EXIT_FAILURE);
+    }
+    exit(EXIT_SUCCESS);
+}

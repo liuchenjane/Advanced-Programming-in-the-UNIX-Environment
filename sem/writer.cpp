16bdd7869baa3fca53500991016dbc2884ba01f9
liuchenjane <2386418764@qq.com>
2017-07-06 08:48:37 +0800

    Update writer.cpp

--- sem/writer.cpp
+++ sem/writer.cpp
@@ -14,29 +14,29 @@ union semun{
 };
 int main(){
     int semid,shmid;
-    shmid = shmget(SHM_KEY, sizeof(int),IPC_CREAT | 0666);
+    shmid = shmget(SHM_KEY, sizeof(int),IPC_CREAT | 0666);//获得同一个共享内存
     if (shmid<0){
         printf("create shm error\n" );
             return -1;
     }
     void * shmptr;
-    shmptr = shmat(shmid,NULL,0);
+    shmptr = shmat(shmid,NULL,0);//连接到与reader同一个共享内存
     if(shmptr == (void *)-1){
         printf("shmat error: %s\n",strerror(errno) );
         return -1;
     }
     int * data = (int *)shmptr;
-    semid = semget(SHM_KEY,2,IPC_CREAT|0666);
+    semid = semget(SHM_KEY,2,IPC_CREAT|0666);//接收与reader相同的信号量
   //  union semun semun1;
     struct sembuf sembuf1;
     while(1){
-        sembuf1.sem_num=1;
-        sembuf1.sem_op=-1;
+        sembuf1.sem_num=1;//对第二个信号量操作
+        sembuf1.sem_op=-1;//-1,reader中将其初始值设为1
         sembuf1.sem_flg=SEM_UNDO;
         semop(semid,&sembuf1,1);
-        scanf("%d",data);
-        sembuf1.sem_num=0;
-        sembuf1.sem_op=1;
+        scanf("%d",data);//用户在终端输入
+        sembuf1.sem_num=0;//对第一个信号量操作
+        sembuf1.sem_op=1;//加1
         sembuf1.sem_flg=SEM_UNDO;
         semop(semid,&sembuf1,1);
     }

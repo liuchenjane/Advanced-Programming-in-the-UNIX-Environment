fccdbd5e04dffd3493b6e254b4ed63e0e1e93566
liuchenjane <2386418764@qq.com>
2017-07-06 08:43:03 +0800

    Update reader.cpp

--- sem/reader.cpp
+++ sem/reader.cpp
@@ -14,32 +14,32 @@ union semun{
 };
 int main(){
     int semid,shmid;
-    shmid = shmget(SHM_KEY, sizeof(int),IPC_CREAT | 0666);
+    shmid = shmget(SHM_KEY, sizeof(int),IPC_CREAT | 0666);//获得共享内存
     if (shmid<0){
         printf("create shm error\n" );
             return -1;
     }
     void * shmptr;
-    shmptr = shmat(shmid,NULL,0);
+    shmptr = shmat(shmid,NULL,0);//把自己连接到该地址空间
     if(shmptr == (void *)-1){
         printf("shmat error: %s\n",strerror(errno) );
         return -1;
     }
     int * data = (int *)shmptr;
-    semid = semget(SHM_KEY,2,IPC_CREAT|0666);
+    semid = semget(SHM_KEY,2,IPC_CREAT|0666);//创建并初始化2个信号量
     union semun semun1;
     semun1.val=0;
-    semctl(semid,0,SETVAL,semun1);
+    semctl(semid,0,SETVAL,semun1);//将第一个信号量设置为0
     semun1.val=1;
-    semctl(semid,1,SETVAL,semun1);
+    semctl(semid,1,SETVAL,semun1);//将第二个信号量设置为1
     struct sembuf sembuf1;
     while(1){
-        sembuf1.sem_num=0;
-        sembuf1.sem_op=-1;
+        sembuf1.sem_num=0;//下面操作的是第一个信号量
+        sembuf1.sem_op=-1;//初始值为0,-1操作等待
         sembuf1.sem_flg=SEM_UNDO;
         semop(semid,&sembuf1,1);
         printf("the NUM:%d\n",*data );
-        sembuf1.sem_num=1;
+        sembuf1.sem_num=1;//让writer再次就绪
         sembuf1.sem_op=1;
         sembuf1.sem_flg=SEM_UNDO;
         semop(semid,&sembuf1,1);
